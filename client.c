41e3560ccf27315f36609da564ecc33332ce808c
helenduz <duh2@carleton.edu>
2021-07-13 12:57:13 +0800

    added files via upload

---
+++ client.c
@@ -0,0 +1,141 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define NAME_SIZE 40
+#define BUFFER_SIZE 2048
+
+char name[NAME_SIZE];
+struct sockaddr_in server_addr;
+int sock;
+// int exit_flag = 0;
+
+
+void print_prompt(){
+    printf("%s", ">>> ");
+    fflush(stdout);
+}
+
+
+/* Thread that handles sending message from client to server */
+void *handle_send(){
+    char message[BUFFER_SIZE - NAME_SIZE - 16];
+    char buffer_send[BUFFER_SIZE];
+    while (1){
+        fgets(message, BUFFER_SIZE - NAME_SIZE - 16, stdin);
+        message[strcspn(message, "\n")] = '\0'; // trims space after newline char
+        // exit if user entered "quit"
+        if (strcmp(message, "quit") == 0){
+            pthread_exit(0);
+        }
+        sprintf(buffer_send, ">>> %s: %s\n", name, message);
+        if (send(sock, buffer_send, strlen(buffer_send), 0) <0){
+            perror("Error occured when sending message to server");
+            continue;
+        }
+        bzero(message, BUFFER_SIZE - NAME_SIZE - 16);
+        bzero(buffer_send, BUFFER_SIZE);
+    }
+}
+
+
+/* Thread that handles receiving messages from server */
+void *handle_recv(){
+    char buffer_recv[BUFFER_SIZE];
+    while (1){
+        int receive = recv(sock, buffer_recv, BUFFER_SIZE, 0);
+        if (receive < 0){
+            sleep(10);
+            perror("Error occured when receiving message from server");
+            continue;
+        }
+        else if (receive == 0){
+            perror("Server side has initiated a shutdown, all messages received");
+            // directly exit the program if server is closed
+            printf("Quitting program now");
+            sleep(5); // wait for send() to print out error to notify user 
+            exit(1);  
+        }
+        printf("%s", buffer_recv);
+        bzero(buffer_recv, BUFFER_SIZE); 
+    }
+}
+
+
+int main(int argc, char **argv){
+    // gather and set server info
+    char *server_ip = argv[1];
+	int server_port = atoi(argv[2]);
+    bzero(&server_addr, sizeof(server_addr)); // zero-init the struct
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(server_port);
+
+    // create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0){
+        perror("Error occured when creating socket");
+    }
+
+    // connect to server (i.e. setting peer address for future send()'s)
+    if (connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+        perror("Error occured when connecting to server");
+        return EXIT_FAILURE;
+    }
+
+    // check if max client is reached
+    char init_buffer[BUFFER_SIZE];
+    recv(sock, init_buffer, BUFFER_SIZE, 0);
+    init_buffer[strcspn(init_buffer, "\n")] = '\0';
+    printf("%s\n", init_buffer);
+    if (init_buffer[0] == 'M'){
+        printf("Quitting program now\n");
+        sleep(5);
+        return EXIT_SUCCESS;
+    }
+
+    // get and send name to server
+    printf("Enter your name: ");
+    fgets(name, NAME_SIZE, stdin);
+    name[strcspn(name, "\n")] = '\0'; // trim spaces after newline
+    /* to do: impose size on name */
+    if (send(sock, name, strlen(name), 0) <0){
+        perror("Error occured when sending user name to server");
+    }
+
+    // create thread for handling receiving messages
+    pthread_t recv_tid;
+    if (pthread_create(&recv_tid, NULL, &handle_recv, NULL) != 0){
+            perror("Error occured when creating receiving thread");
+    }   
+
+    // create thread for handling sending messages
+    pthread_t send_tid;
+    if (pthread_create(&send_tid, NULL, &handle_send, NULL) != 0){
+            perror("Error occured when creating receiving thread");
+    }  
+
+    // // exit when exit_flag is set
+    // while (1){
+    //     if (exit_flag = 1){
+    //         close(sock);
+    //         return EXIT_SUCCESS;
+    //     }
+    // }
+    void *status;
+    if (pthread_join(send_tid, &status) != 0){
+        perror("Error occured during pthread_join() for handle_send");
+    }
+    if (close(sock) < 0){
+        perror("Error occured when closing socket");
+    } // probably not needed since "quit" will cause server side to initiate close()
+    return EXIT_SUCCESS;
+}
---
+++ server.c
@@ -0,0 +1,224 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#define MAX_CLIENTS 3
+#define NAME_SIZE 40
+#define BUFFER_SIZE 2048
+#define SERVER_PORT 6666
+
+/* structure to store client info */
+typedef struct{
+	struct sockaddr_in addr;
+	int client_socket; // file descriptor, from accept()
+	int client_id;
+	char client_name[NAME_SIZE];
+} client_t;
+client_t *client_list[MAX_CLIENTS]; 
+struct sockaddr_in server_addr; // socketaddr_in stores information about an IP socket
+int server_socket; // file descriptor, from socket()
+static volatile int client_count;
+pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER; // separate dequeque & enqueue & ensure_max
+
+
+/* returns a file descriptor for a listening socket to be used by server */
+int get_listen_socket(){
+    // create server socket file descriptor
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0){
+        perror("Error occured when creating server socket\n");
+        return EXIT_FAILURE;
+    }
+
+    // fill in info about server socket address
+    bzero(&server_addr, sizeof(server_addr)); // zero-init the struct
+    server_addr.sin_family = AF_INET;                
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //INADDR_ANY means 0.0.0.0 (all available interfaces)
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    // set socket option to allow reuse of local address
+    // i.e. allow the port to be used again immediately instead of waiting for timeouts on the TCP stack
+    int option = 1;  
+    if((setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)))<0){  
+        perror("Error occured in setsockopt()\n");  
+        return EXIT_FAILURE; 
+    }
+
+    // bind file descriptor to address
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        perror("Error occured when binding server socket\n");
+        return EXIT_FAILURE;
+    }
+
+    // set socket to listen (able to accept connections)
+    if (listen(server_socket, MAX_CLIENTS) < 0){
+        perror("Error occured when setting server socket to listen\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("LISTENING PORT ESTABLISHED: %d\n", SERVER_PORT);
+    return server_socket;    
+}
+
+
+/* adds a new (pointer to) client struct into the client_list 
+*  due to the multi-thread approach, no guarantee that clients will be added in order
+*/
+void enqueue(client_t *new_client_struct){
+    pthread_mutex_lock(&client_mutex);
+    for(int i = 0; i < MAX_CLIENTS; i++){
+		if (!client_list[i]){ // note: global variables automatically initialized to zero
+			client_list[i] = new_client_struct;
+			break;
+		}
+	}
+    client_count++;
+    pthread_mutex_unlock(&client_mutex);
+}
+
+
+/* adds a new (pointer to) client struct into the client_list */
+void dequeque(int client_id){
+	pthread_mutex_lock(&client_mutex);
+	for(int i = 0; i < MAX_CLIENTS; i++){
+		if (client_list[i]){
+			if (client_list[i]->client_id == client_id){
+				client_list[i] = NULL;
+				break;
+			}
+		}
+	}
+    client_count--;
+	pthread_mutex_unlock(&client_mutex);
+}
+
+
+/* send content in buffer to all client sockets except for the sender itself */
+void send_message(char *buffer, int sender_client_id){
+	pthread_mutex_lock(&client_mutex);
+	for(int i = 0; i < MAX_CLIENTS; i++){
+		if (client_list[i] && (client_list[i]->client_id != sender_client_id)){
+            if (send(client_list[i]->client_socket, buffer, strlen(buffer), 0) < 0){
+                perror("Error occured when sending message to client\n");
+                break;
+            }
+		}
+	}
+	pthread_mutex_unlock(&client_mutex);   
+}
+
+
+/* thread responsible for handling communication between server and a client 
+*  (from entering the name to client quiting the connection)
+*/
+void *handle_one_client(void *arg){
+    client_t *cur_client = (client_t *) arg;
+    char buffer[BUFFER_SIZE];
+    char name[NAME_SIZE];
+    int exit_flag = 0;
+
+    // capture the client's name
+	if(recv(cur_client->client_socket, name, NAME_SIZE, 0) <= 0){
+		exit_flag = 1;
+	}
+    else{
+		strcpy(cur_client->client_name, name);
+		sprintf(buffer, "--- %s has joined ---\n", cur_client->client_name);
+		printf("%s", buffer);
+		send_message(buffer, cur_client->client_id);
+	}
+
+    bzero(buffer, BUFFER_SIZE);
+
+    // wait for message to arrive from client
+    while(1){
+        if (exit_flag){
+            break;
+        }
+        int receive = recv(cur_client->client_socket, buffer, BUFFER_SIZE, 0);
+        // when client has ended connection or typed exit
+        if (receive == 0 || strcmp(buffer, "quit") == 0){
+            sprintf(buffer, "--- %s has left ---\n", cur_client->client_name);
+            printf("%s", buffer);
+            send_message(buffer, cur_client->client_id);
+            exit_flag = 1;
+        }
+        else if (receive > 0){
+            send_message(buffer, cur_client->client_id);
+            printf("%s", buffer);
+        }
+        // if receive = -1 then error has occured
+		else{
+			printf("Error occured when receiving client message\n");
+			exit_flag = 1;
+		}
+		bzero(buffer, BUFFER_SIZE); // empty buffer after every message
+    }
+
+    close(cur_client->client_socket);
+    dequeque(cur_client->client_id);
+    free(cur_client);
+    pthread_exit(0);
+}
+
+
+int main(int argc, char **argv){
+    client_count = 0;
+    pthread_t tid; // thread id (overwritten for every new thread created)
+    struct sockaddr_in client_addr; // overwritten for every new client connected
+    int client_socket; // file descriptor, from accept() 
+    int client_id = 0; // will be assigned as client_id's (note: != index to client_list)
+
+    server_socket = get_listen_socket();
+
+    while (1){
+        // accept() will wait for client connection (create new socket + allocates new file descriptor)
+        int addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *) &client_addr, (socklen_t *) &addr_len);
+        if (client_socket < 0){
+            perror("Error occured when accepting client\n");
+            continue;
+        }
+
+        // ensure max client is not exceeded
+        pthread_mutex_lock(&client_mutex);
+        if (client_count == MAX_CLIENTS){
+            char max_error[32];
+            sprintf(max_error, "Max clients (%d) reached.\n", MAX_CLIENTS);
+            send(client_socket, max_error, strlen(max_error), 0);
+            printf("%s", max_error);
+            printf("Rejected client: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            close(client_socket);
+            continue;       
+        }
+        else{
+            char conn_success[96];
+            sprintf(conn_success, "-------Chat Room Connection Successful. Type quit to exit--------\n");
+            send(client_socket, conn_success, strlen(conn_success), 0);
+        }
+        pthread_mutex_unlock(&client_mutex);
+
+        printf("New client connection: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        // fill in client struct for the new client
+		client_t *new_client_struct = (client_t *) malloc(sizeof(client_t));
+		new_client_struct->addr = client_addr;
+		new_client_struct->client_socket = client_socket;
+		new_client_struct->client_id = client_id++;
+
+		/* Add client to the queue and create thread */
+		enqueue(new_client_struct);
+		if (pthread_create(&tid, NULL, &handle_one_client, (void *) new_client_struct) != 0){
+            perror("Error occured when creating client thread\n");
+        }   
+    }   
+    return EXIT_SUCCESS;
+}
